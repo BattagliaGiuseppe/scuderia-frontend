f813ab3288b60cf6983b4e4ab3837386460d1377
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-17 11:32:23 +0200

    Delete src directory

--- src/pages/Races.jsx
+++
@@ -1,8 +0,0 @@
-export default function Races() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Calendario Gare</h1>
-      <p>Qui sarà possibile aggiungere le gare e assegnare i veicoli.</p>
-    </div>
-  );
-}
