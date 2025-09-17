e78c6701db9039c8b24b5b4a848194eab4656cc0
ChatGPT-temp-BRC <info@superf1000.it>
2025-09-17 20:04:37 +0200

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,19 @@
 import axios from 'axios';
-const API_URL = 'https://scuderia-backend.onrender.com/api';
-export default axios.create({
-  baseURL: API_URL
+
+const instance = axios.create({
+  baseURL: 'https://scuderia-backend.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
+
+// Se hai bisogno di aggiungere il token in automatico a ogni richiesta:
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default instance;
