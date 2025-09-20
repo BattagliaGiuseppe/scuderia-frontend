a8fb4dc9ce4f8b7fcdd7167505fe250b7d615402
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-20 17:23:37 +0200

    Update expiringParts.js

--- src/api/expiringParts.js
+++ src/api/expiringParts.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const expiringPartsApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,  // niente fallback localhost
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default expiringPartsApi;
