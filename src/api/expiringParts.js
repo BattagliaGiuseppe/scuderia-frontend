101c469cc368e23be90b221d87f463e4345f44fe
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-20 17:22:54 +0200

    Update expiringParts.js

--- src/api/expiringParts.js
+++ src/api/expiringParts.js
@@ -1,8 +1,7 @@
-// src/api/expiringParts.js
 import axios from 'axios';
 
-export const getExpiringParts = (token) => {
-  return axios.get('/expiring_parts', {
-    headers: { Authorization: `Bearer ${token}` }
-  });
-};
+const expiringPartsApi = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,  // niente fallback localhost
+});
+
+export default expiringPartsApi;
