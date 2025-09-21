972316bc5c5130cadd84654a51d897d8cf3a4f86
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-21 23:03:53 +0200

    Update vehicles.js

--- src/api/vehicles.js
+++ src/api/vehicles.js
@@ -1,21 +1,15 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'https://scuderia-backend.onrender.com/api',
-});
+const API_URL = 'https://scuderia-backend.onrender.com/api';
 
-// Richiesta GET di tutti i veicoli
 export const getVehicles = (token) => {
-  return api.get('/vehicles', {
-    headers: { Authorization: `Bearer ${token}` },
+  return axios.get(`${API_URL}/vehicles`, {
+    headers: { Authorization: `Bearer ${token}` }
   });
 };
 
-// Puoi aggiungere altre funzioni per POST, PUT, DELETE ecc.
-export const addVehicle = (token, vehicleData) => {
-  return api.post('/vehicles', vehicleData, {
-    headers: { Authorization: `Bearer ${token}` },
+export const createVehicle = (token, data) => {
+  return axios.post(`${API_URL}/vehicles`, data, {
+    headers: { Authorization: `Bearer ${token}` }
   });
 };
-
-export default api;
