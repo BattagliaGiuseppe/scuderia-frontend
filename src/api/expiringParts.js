7a1aa19f51a822688ec80c8d8da01512b8724e8e
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-21 23:10:10 +0200

    Update expiringParts.js

--- src/api/expiringParts.js
+++ src/api/expiringParts.js
@@ -4,9 +4,15 @@ const expiringPartsApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL + '/api',
 });
 
-// funzione che chiama l'endpoint corretto delle parti in scadenza
 export const getExpiringParts = async (token) => {
   const response = await expiringPartsApi.get('/expiring_parts', {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+};
+
+export const createExpiringPart = async (token, data) => {
+  const response = await expiringPartsApi.post('/expiring_parts', data, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
