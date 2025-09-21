181b7508c1a754362b50576ebde6d0add4d3ed8e
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-21 23:09:48 +0200

    Update maintenances.js

--- src/api/maintenances.js
+++ src/api/maintenances.js
@@ -1,16 +1,21 @@
-// src/api/maintenances.js
 import axios from 'axios';
 
-const API_URL = 'https://scuderia-backend.onrender.com/api';
+const maintenancesApi = axios.create({
+  baseURL: import.meta.env.VITE_API_URL + '/api',
+});
 
-export const getMaintenances = (token) => {
-  return axios.get(`${API_URL}/maintenances`, {
-    headers: { Authorization: `Bearer ${token}` }
+export const getMaintenances = async (token) => {
+  const response = await maintenancesApi.get('/maintenances', {
+    headers: { Authorization: `Bearer ${token}` },
   });
+  return response.data;
 };
 
-export const createMaintenance = (token, data) => {
-  return axios.post(`${API_URL}/maintenances`, data, {
-    headers: { Authorization: `Bearer ${token}` }
+export const createMaintenance = async (token, data) => {
+  const response = await maintenancesApi.post('/maintenances', data, {
+    headers: { Authorization: `Bearer ${token}` },
   });
+  return response.data;
 };
+
+export default maintenancesApi;
