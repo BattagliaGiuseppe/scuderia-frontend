687fd95a4160b581edc9d88c61852efc4477fdfd
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-21 23:01:18 +0200

    Update maintenances.js

--- src/api/maintenances.js
+++ src/api/maintenances.js
@@ -1,14 +1,16 @@
 // src/api/maintenances.js
 import axios from 'axios';
 
+const API_URL = 'https://scuderia-backend.onrender.com/api';
+
 export const getMaintenances = (token) => {
-  return axios.get('/maintenances', {
+  return axios.get(`${API_URL}/maintenances`, {
     headers: { Authorization: `Bearer ${token}` }
   });
 };
 
 export const createMaintenance = (token, data) => {
-  return axios.post('/maintenances', data, {
+  return axios.post(`${API_URL}/maintenances`, data, {
     headers: { Authorization: `Bearer ${token}` }
   });
 };
