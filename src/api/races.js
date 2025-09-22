19ae9f2ea5abb502e4a1ae51a1c9c8f55311545f
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-22 18:14:30 +0200

    Update races.js

--- src/api/races.js
+++ src/api/races.js
@@ -4,4 +4,11 @@ const racesApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+export const getRaces = async (token) => {
+  const response = await racesApi.get('/api/races', {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return response.data;
+};
+
 export default racesApi;
