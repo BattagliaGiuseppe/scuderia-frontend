792ac380f7c5c10af64e27595b6afa34cf56f006
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-22 18:14:13 +0200

    Update maintenances.js

--- src/api/maintenances.js
+++ src/api/maintenances.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 const maintenancesApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const getMaintenances = async (token) => {
-  const response = await maintenancesApi.get('/maintenances', {
+  const response = await maintenancesApi.get('/api/maintenances', {
     headers: { Authorization: `Bearer ${token}` }
   });
   return response.data;
 };
 
 export const createMaintenance = async (token, data) => {
-  const response = await maintenancesApi.post('/maintenances', data, {
+  const response = await maintenancesApi.post('/api/maintenances', data, {
     headers: { Authorization: `Bearer ${token}` }
   });
   return response.data;
