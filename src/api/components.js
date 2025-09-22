2334bc8912130e06e7618284bd520075f9072758
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-22 18:13:19 +0200

    Update components.js

--- src/api/components.js
+++ src/api/components.js
@@ -1,19 +1,12 @@
 import axios from 'axios';
 
 const componentsApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const getComponents = async (token) => {
-  const response = await componentsApi.get('/components', {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  return response.data;
-};
-
-export const createComponent = async (token, data) => {
-  const response = await componentsApi.post('/components', data, {
-    headers: { Authorization: `Bearer ${token}` },
+  const response = await componentsApi.get('/api/components', {
+    headers: { Authorization: `Bearer ${token}` }
   });
   return response.data;
 };
