556db8ee0885fe92a78c9ac1c09d13ca1bd14125
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-22 18:13:57 +0200

    Update jusers.js

--- src/api/jusers.js
+++ src/api/jusers.js
@@ -4,4 +4,11 @@ const usersApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+export const getUsers = async (token) => {
+  const response = await usersApi.get('/api/users', {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return response.data;
+};
+
 export default usersApi;
