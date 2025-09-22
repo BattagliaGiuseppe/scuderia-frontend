c5f98368197ea530fb39103b75c1afdd5803b4ac
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-22 18:14:45 +0200

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const authApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const login = async (username, password) => {
-  const response = await authApi.post('/login', { username, password });
+  const response = await authApi.post('/api/login', { username, password });
   return response.data;
 };
 
 export const register = async (username, password, role) => {
-  const response = await authApi.post('/register', { username, password, role });
+  const response = await authApi.post('/api/register', { username, password, role });
   return response.data;
 };
 
