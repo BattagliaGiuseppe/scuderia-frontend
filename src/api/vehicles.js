fb5dcdbf5b7f62ec4c9ee4a927e18e9347479f97
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-22 18:16:04 +0200

    Update vehicles.js

--- src/api/vehicles.js
+++ src/api/vehicles.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 const vehiclesApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api', // aggiunge /api qui
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const getVehicles = async (token) => {
-  const response = await vehiclesApi.get('/vehicles', {
+  const response = await vehiclesApi.get('/api/vehicles', {
     headers: { Authorization: `Bearer ${token}` }
   });
   return response.data;
 };
 
 export const createVehicle = async (token, data) => {
-  const response = await vehiclesApi.post('/vehicles', data, {
+  const response = await vehiclesApi.post('/api/vehicles', data, {
     headers: { Authorization: `Bearer ${token}` }
   });
   return response.data;
